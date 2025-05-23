915de316ebafcedbdb1e9ca90621d0c8769d1c0e
ESCANOR-001 <sg12102002@gmail.com>
2025-05-23 23:35:00 +0530

    Error Fixed

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Fixes npm packages that depend on `crypto` module
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        buffer: require.resolve('buffer'),
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
